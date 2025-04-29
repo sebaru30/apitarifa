0da422fea530e312dd76cdbe79ef23ddc73ed9b4
sebaru30 <vargas.23.cvc@gmail.com>
2025-04-28 22:30:29 -0500

    actualizada del crud de la tabla tarifa

--- src/controller/tarifacontroller.js
+++ src/controller/tarifacontroller.js
@@ -28,3 +28,117 @@ export const obtenertarifaID = async (req,res)=>
          //resultado del navegador
          res.json(resultado.recordset);
     }
+
+    
+
+        export const insertarnuevatarifa = async (req,res)=>
+    
+            {
+          
+              const {descripcionTarifa,costoTarifa} = req.body;
+          
+              //validamos que existan los parametros en el body 
+          
+          
+          
+              if(descripcionTarifa==null || costoTarifa==null )
+          
+              {
+          
+                return res.status(500).json({msg: "parametros insuficientes.."});
+          
+              }
+          
+              const conexion = await obtenerConexion();
+          
+              const resultado = await conexion.request()
+          
+                .input('descripcionTarifa',descripcionTarifa)
+          
+                .input('costoTarifa',costoTarifa)
+          
+                .query("insert into tarifas(descripcionTarifa,costoTarifa) values (@descripcionTarifa,@costoTarifa);")
+          
+                 //imprimir consola (DEV)
+          
+              console.log(resultado);
+          
+              //Resultado al navegador 
+          
+              res.json({descripcionTarifa,costoTarifa});
+            }
+    
+            export const borrartarifa = async (req,res)=>
+    
+                {
+            
+                  //recibimos idtarifa como parametro que llega por la URL
+            
+                  const{idtarifa}=req.params;
+                  const conexion = await obtenerConexion();
+                  const resultado = await conexion.request()
+            
+                    .input('idtarifa',sql.Int,idtarifa)
+            
+                    .query("DELETE FROM tarifas where idtarifa=@idtarifa")
+                    //imprimir consola (DEV)
+            
+                    console.log(resultado);
+            
+                    //Resultado al navegador 
+            
+                    res.json(resultado.recordset);
+            
+                }
+    
+                export const actualizartarifa = async (req, res) => {
+    
+                  try {
+                
+                    const { idtarifa } = req.params;
+                
+                    const { descripcionTarifa, costoTarifa } = req.body;
+                
+                
+                
+                    // Validación simple
+                
+                    if (!descripcionTarifa || !costoTarifa) {                
+                      return res.status(400).json({ message: 'Todos los campos son obligatorios' });                
+                    }
+                    const conexion = await obtenerConexion();                    
+                    const resultado = await conexion.request()
+
+                       
+                        .input('idtarifa', sql.Int, idtarifa)
+                        .input('descripcionTarifa', sql.VarChar, descripcionTarifa)
+                    
+                        .input('costoTarifa', sql.Float,  costoTarifa)                   
+                    
+                        .query(`
+                
+                        UPDATE tarifas 
+                
+                        SET 
+                
+                          descripcionTarifa = @descripcionTarifa,
+                
+                          costoTarifa = @costoTarifa
+                
+                        WHERE idtarifa = @idtarifa 
+                
+                      `);
+                
+                
+                
+                    res.json({ message: 'Registro actualizado exitosamente' });
+                
+                  } catch (error) {
+                
+                    console.error('Error al actualizar la tarifa:', error);
+                
+                    res.status(500).json({ message: 'Error al actualizar tarifa' });
+                
+                  }
+                
+                }
--- src/routes/tarifa.routes.js
+++ src/routes/tarifa.routes.js
@@ -1,11 +1,17 @@
 import { Router} from "express";
 const rutas = Router();
 
-import {obtenertarifa,obtenertarifaID } from '../controller/tarifacontroller.js';
+import {obtenertarifa,obtenertarifaID,insertarnuevatarifa, borrartarifa,actualizartarifa} from '../controller/tarifacontroller.js';
 //esta API devuelve una lista de todos las tarifas de la BD
 rutas.get('/tarifas',obtenertarifa);
 
 //esta API me devuelve los datos de un tarifa en funcion del ID
 rutas.get('/obtenertarifaID/:idtarifa',obtenertarifaID);
+
+rutas.post('/insertartarifa',insertarnuevatarifa);
+
+rutas.get('/borrartarifa/:idtarifa', borrartarifa);
+
+rutas.put('/tarifas/:idtarifa', actualizartarifa); 
 
 export default rutas;
